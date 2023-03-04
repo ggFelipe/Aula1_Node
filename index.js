e5fd423568bff7a8a9beef18cc6017b3fe849a4b
Christian Felipe Oller de Oliveira <christianfelipe_09@hotmail.com>
2023-03-03 21:16:58 -0300

    Aula1_Node

---
+++ index.js
@@ -0,0 +1,38 @@
+import PromptSync from "prompt-sync"
+
+const prompt = PromptSync()
+
+/* const nome = prompt("Digite seu Nome: ")
+
+console.log(`Olá ${nome}!`)
+
+const temp = Number(prompt("Digite a temperatura: "))
+
+if(temp > 38){
+    console.log("Febre")
+} else if (temp > 26){
+    console.log("F")
+} 
+else{
+    console.log("OK")
+} */
+
+const salario = Number(prompt("Dgite salario: "))
+const aumento = Number(prompt("Digite o aumento: "))
+
+const novoSalario = salario * (1 + (aumento/100))
+
+console.log(`Novo Salario e ${novoSalario}`)
+
+const age = Number(prompt("digite seu ano de nascimento: "))
+const ano = new Date()
+
+const idade = ano.getFullYear() - age
+
+console.log(`Idade e ${idade}`)
+
+if(idade >= 18){
+    console.log("Pai vai Pilotar")
+}else{
+    console.log("Só em cardeal")
+}
